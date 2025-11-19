eeb9eefab0c68600b99e102c6e0a7e7ad54177a5
jiegomojica <boh618@gmail.com>
2025-11-19 11:34:15 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Tradecalc.jsx'
+import App from './TradeCalc.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
